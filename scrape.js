ef79de13f1c9a421f71f2be073052003c46d7412
Lisa Stasio <lstasio@sgintl.com>
2019-05-07 13:03:09 -0400

    added logic to connect to mongo db

--- scrape.js
+++ scrape.js
@@ -1,7 +1,47 @@
 var cheerio = require("cheerio");
 var axios = require("axios");
 
-// Use axios to search NPR site
+var express = require("express");
+var mongojs = require("mongojs");
+
+var app = express();
+
+var databaseURL = "news";
+var collection = ["scrapedNews"];
+
+var db = mongojs(databaseUrl, collections);
+
+// log any errors
+db.on("error", function(error) {
+  console.log("Database Error:", error);
+});
+
+app.get("/", function(req, res) {
+  res.send("Hello world");
+});
+
+
+// Get data from Mongo DB
+
+app.get("/all", function(req, res) {
+// Find all results from the scrapedData collection in the db
+db.scrapedData.find({}, function(error, found) {
+  // Throw any errors to the console
+    if (error) {
+      console.log(error);
+    }
+    // If there are no errors, send the data to the browser as json
+    else {
+      res.json(found);
+    }
+  });
+});
+
+
+// Use axios to search NPR site and scrape into MongoDB
+
+app.get("/scrape", function(req, res) {
+
 axios.get("https://www.npr.org/sections/world/").then(function(response) {
 
 var $ = cheerio.load(response.data);
@@ -13,12 +53,23 @@ var results = [];
   $("h2.title").each(function(i, element) {
 
     var title = $(element).text();
+    //Come back to this to pull summary and URL
+    //var summary = $(element)
 
-    results.push({
+    db.scrapedData.insert({
       title: title   
       
     });
   });
 
-  console.log(results);
+  console.log(inserted);
+});
+
+
+res.send("Scrape Complete");
+});
+
+
+app.listen(3000, function() {
+  console.log("App running on port 3000!");
 });
